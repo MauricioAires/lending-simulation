0e4409139a0b4b298ac17e6cb5d262df85cffa75
MauricioAires <mauricioaires015@gmail.com>
2024-02-25 02:29:04 -0300

    feat(routes) config

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { SuccessLeanComponent } from './pages/success-lean/success-lean.component';
+import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'sucesso', component: SuccessLeanComponent },
+  { path: '**', component: HomeComponent },
+];
