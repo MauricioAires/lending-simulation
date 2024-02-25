4247deb0ff121d85c23167d786fc870e18f7354b
MauricioAires <mauricioaires015@gmail.com>
2024-02-25 01:47:47 -0300

    feat(loan popup) remove mock and use props

--- src/app/confirm-loan-popup/confirm-loan-popup.component.ts
+++ src/app/confirm-loan-popup/confirm-loan-popup.component.ts
@@ -1,6 +1,14 @@
 import { NgIf } from '@angular/common';
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 
+interface ILoanPayload {
+  loanAmount: number;
+  loanAmountFormatted: string;
+  numberOfInstallments: number;
+  totalAmountWithInterestFormatted?: string;
+  valueOfInstallmentsFormatted?: string;
+}
+
 @Component({
   selector: 'app-confirm-loan-popup',
   standalone: true,
@@ -8,6 +16,9 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
   templateUrl: './confirm-loan-popup.component.html',
 })
 export class ConfirmLoanPopupComponent {
+  @Input('data')
+  public loanPayload: ILoanPayload = {} as ILoanPayload;
+
   @Input('show')
   public show = true;
 
