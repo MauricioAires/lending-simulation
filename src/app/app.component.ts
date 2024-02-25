da3a8aa104ed89edfb8b7439a1617f0a7f1df774
MauricioAires <mauricioaires015@gmail.com>
2024-02-25 01:47:59 -0300

    feat(home) add loan service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,40 @@
 import { Component } from '@angular/core';
 import { Validators } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
-import { Location } from '@angular/common';
+import { NgIf } from '@angular/common';
 import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 import { ConfirmLoanPopupComponent } from './confirm-loan-popup/confirm-loan-popup.component';
+import { LoanService } from './services/loan.service';
+import { HttpClientModule } from '@angular/common/http';
+
+interface ILoanPayload {
+  loanAmount: number;
+  loanAmountFormatted: string;
+  numberOfInstallments: number;
+  valueOfInstallmentsFormatted?: string;
+  totalAmountWithInterestFormatted?: string;
+}
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ConfirmLoanPopupComponent, ReactiveFormsModule],
+  imports: [
+    RouterOutlet,
+    NgIf,
+    ConfirmLoanPopupComponent,
+    HttpClientModule,
+    ReactiveFormsModule,
+  ],
+  providers: [LoanService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
+  public isLoadingGenerateSimulation = false;
   public showConfirmLoan = false;
   public submitted = false;
-  public loanPayload = {
+  public loanPayload: ILoanPayload = {
     loanAmount: 0,
     loanAmountFormatted: '',
     numberOfInstallments: 0,
@@ -27,11 +46,14 @@ export class AppComponent {
     numberOfInstallments: [0, Validators.required],
   });
 
-  constructor(private formBuilder: FormBuilder, private location: Location) {}
+  constructor(
+    private loanService: LoanService,
+    private formBuilder: FormBuilder
+  ) {}
 
   public handleShowConfirmLoan() {
     if (!this.submitted) {
-      this.scrollToAnchor();
+      this.scrollToAnchor('calcular-emprestimo-pessoal');
 
       return;
     }
@@ -43,28 +65,53 @@ export class AppComponent {
     this.showConfirmLoan = false;
   }
 
-  private scrollToAnchor() {
-    this.location.go('#calcular-emprestimo-pessoal');
-
-    const elementRef = document.getElementById('calcular-emprestimo-pessoal');
+  private scrollToAnchor(anchorId: string) {
+    const elementRef = document.getElementById(anchorId);
 
     if (elementRef) {
       elementRef.scrollIntoView();
     }
   }
 
-  public onSubmit() {
-    this.submitted = true;
+  private currencyFormatter(value: number) {
+    return new Intl.NumberFormat('pt-BR', {
+      currency: 'BRL',
+      style: 'currency',
+    }).format(value);
+  }
 
+  public onSubmit() {
+    this.isLoadingGenerateSimulation = true;
     const { loanAmount, numberOfInstallments } = this.loanForm.value;
 
-    this.loanPayload = {
-      loanAmountFormatted: new Intl.NumberFormat('pt-BR', {
-        currency: 'BRL',
-        style: 'currency',
-      }).format(Number(loanAmount)),
+    const totalAmountWithInterest =
+      (Number(loanAmount) * 5) / 100 + Number(loanAmount);
+
+    const payload = {
       loanAmount: Number(loanAmount),
       numberOfInstallments: Number(numberOfInstallments),
+      valueOfInstallments:
+        totalAmountWithInterest / Number(numberOfInstallments),
+      totalAmountWithInterest: totalAmountWithInterest,
     };
+
+    this.loanService.generateSimulation(payload).subscribe((response) => {
+      this.submitted = true;
+      this.isLoadingGenerateSimulation = false;
+
+      this.loanPayload = {
+        loanAmountFormatted: this.currencyFormatter(response.loanAmount),
+        loanAmount: response.loanAmount,
+        numberOfInstallments: response.numberOfInstallments,
+        totalAmountWithInterestFormatted: this.currencyFormatter(
+          totalAmountWithInterest
+        ),
+        valueOfInstallmentsFormatted: this.currencyFormatter(
+          totalAmountWithInterest / response.numberOfInstallments
+        ),
+      };
+
+      this.scrollToAnchor('contratar-emprestimo');
+    });
   }
 }
